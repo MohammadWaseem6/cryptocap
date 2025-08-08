4ba8e7d4fc38767d49d3365af2e12aeeb8cb2620
MohammadWaseem6 <mohammadwaseem001100@gmail.com>
2025-08-08 19:52:10 +0300

    navbar

--- cryptocap/src/pages/Navbar.tsx
+++ cryptocap/src/pages/Navbar.tsx
@@ -13,29 +13,42 @@ const Navbar: React.FC = () => {
                 </div>
                 <div className='col-span-8 mt-5 '>
                     <ul className='flex  items-center justify-center space-x-20 '>
-                        <li className='hover:text-blue-600'>Home</li>
-                        <li>Business</li>
-                        <li>Trade</li>
-                        <li>Market</li>
-                        <li>Learn</li>
+                        <li className='hover:text-blue-600 cursor-pointer'>Home</li>
+                        <li className='hover:text-blue-600 cursor-pointer'>Business</li>
+                        <li className='hover:text-blue-600 cursor-pointer'>Trade</li>
+                        <li className='hover:text-blue-600 cursor-pointer'>Market</li>
+                        <li className='hover:text-blue-600 cursor-pointer'>Learn</li>
                     </ul>
                 </div>
                 <div className='col-span-2 flex mt-5'>
-                    <CiGlobe 
-                    className='mr-3.5 text-2xl'
+                    <CiGlobe
+                        className='mr-3.5 text-2xl'
                     />
                     <p className='font-bold'>En </p>
-                    <IoIosArrowDown 
-                    className='mt-1 ml-1'
+                    <IoIosArrowDown
+                        className='mt-1 ml-1'
                     />
                     <button className='bg-blue-400 w-[50px] h-[30px] ml-6 rounded-sm'>Login</button>
                 </div>
-                
+
 
 
 
 
             </div>
+            <div className='text-gray-400  w-full h-[200px] grid items-center justify-items-center  py-70  '>
+                <div className=''>
+                    <h1 className='flex text-4xl font-bold tracking-wider'>Start and Build Your Crypto Portfolio Here</h1>
+
+                </div>
+                <div className='flex items-center justify-items-center text-2xl'>
+                    <p className='tracking-wider mt-4 text-center text-gray-400'>Only at CryptoCap, you can  build a good portfolio  <br />
+                        and learn best practices about CrypotCurrency</p>
+                </div>
+
+                <button className='mt-11 bg-blue-400 rounded-sm text-white w-[100px] h-[50px] cursor-pointer hover:bg-gray-700 hover:text-white '>Get Started</button> 
+            </div>
+             
 
         </div>
     )
